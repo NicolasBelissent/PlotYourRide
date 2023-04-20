6811fb7a7f749eb1c8e4301b899e179945aead47
NicolasBelissent <nicolas.belissent@gmail.com>
2023-04-20 16:36:13 +0100

    add skeleton

---
+++ apps/visualise.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+from streamlit_folium import folium_static
+import folium
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+
+
+
+
+def app():
+
+    st.title("Visualise your Ride")
+
+    st.markdown(
+        """
+        More dev work to come
+    """
+    )
