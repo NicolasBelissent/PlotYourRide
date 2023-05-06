45676123ed36e8d52d8022f32173a73fad966db2
Morek-arthur <arthur@morek.co.uk>
2023-05-06 18:13:57 +0100

    tests

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from apps import home, visualise  # import your app modules here
 
-st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
+st.set_page_config(page_title="Streamlit Geospatial", 
+                   page_icon=":face_vomiting:", layout="wide")
 
 # A dictionary of apps in the format of {"App title": "App icon"}
 # More icons can be found here: https://icons.getbootstrap.com
